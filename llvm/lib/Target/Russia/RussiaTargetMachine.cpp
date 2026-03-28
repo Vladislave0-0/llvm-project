be0df45f34507f7c6745c5c5cb5e8dd9c134b3bd
Vlad Lazar <lazar_2004@list.ru>
2026-03-28 11:39:42 +0300

    [Sim] 6. Add empty RussiaPassConfig

--- llvm/lib/Target/Russia/RussiaTargetMachine.cpp
+++ llvm/lib/Target/Russia/RussiaTargetMachine.cpp
@@ -1,6 +1,7 @@
 #include "RussiaTargetMachine.h"
 #include "Russia.h"
 #include "TargetInfo/RussiaTargetInfo.h"
+#include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/MC/TargetRegistry.h"
 #include <optional>
 
@@ -23,4 +24,25 @@ RussiaTargetMachine::RussiaTargetMachine(const Target &T, const Triple &TT,
           Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
   RUSSIA_DUMP_CYAN
   initAsmInfo();
+}
+
+namespace {
+
+/// Russia Code Generator Pass Configuration Options.
+class RussiaPassConfig : public TargetPassConfig {
+public:
+  RussiaPassConfig(RussiaTargetMachine &TM, PassManagerBase &PM)
+      : TargetPassConfig(TM, PM) {}
+
+  bool addInstSelector() override {
+    RUSSIA_DUMP_CYAN
+    return false;
+  }
+};
+
+} // end anonymous namespace
+
+TargetPassConfig *RussiaTargetMachine::createPassConfig(PassManagerBase &PM) {
+  RUSSIA_DUMP_CYAN
+  return new RussiaPassConfig(*this, PM);
 }
