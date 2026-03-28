8ad4f65f53a4f5dbbdeea888cdc678b191a64fbd
Vlad Lazar <lazar_2004@list.ru>
2026-03-28 16:30:27 +0300

    [Sim] 8. Add empty RussiaAsmPrinter
    Now the backend can finalize the compilation process by converting
    compiler-internal instructions into target-specific machine code.

---
+++ llvm/lib/Target/Russia/SimAsmPrinter.cpp
@@ -0,0 +1,61 @@
+#include "MCTargetDesc/RussiaInstPrinter.h"
+#include "Russia.h"
+#include "RussiaSubtarget.h"
+#include "RussiaTargetMachine.h"
+#include "TargetInfo/RussiaTargetInfo.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+#include "llvm/CodeGen/MachineConstantPool.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstr.h"
+#include "llvm/CodeGen/MachineModuleInfo.h"
+#include "llvm/MC/MCAsmInfo.h"
+#include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCStreamer.h"
+#include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/TargetRegistry.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
+
+using namespace llvm;
+
+#define DEBUG_TYPE "asm-printer"
+
+namespace {
+
+class RussiaAsmPrinter : public AsmPrinter {
+  const MCSubtargetInfo *STI;
+
+public:
+  explicit RussiaAsmPrinter(TargetMachine &TM,
+                         std::unique_ptr<MCStreamer> Streamer)
+      : AsmPrinter(TM, std::move(Streamer)), STI(TM.getMCSubtargetInfo()) {
+    RUSSIA_DUMP_GREEN
+  }
+
+  void emitInstruction(const MachineInstr *MI) override;
+
+  StringRef getPassName() const override { return "Russia Assembly Printer"; }
+
+  bool lowerPseudoInstExpansion(const MachineInstr *MI, MCInst &Inst);
+};
+
+} // end anonymous namespace
+
+// Simple pseudo-instructions have their lowering (with expansion to real
+// instructions) auto-generated.
+#include "RussiaGenMCPseudoLowering.inc"
+
+void RussiaAsmPrinter::emitInstruction(const MachineInstr *MI) {
+  RUSSIA_DUMP_GREEN
+  // Do any auto-generated pseudo lowerings.
+  if (MCInst OutInst; lowerPseudoInstExpansion(MI, OutInst)) {
+    EmitToStreamer(*OutStreamer, OutInst);
+    return;
+  }
+}
+
+// Force static initialization.
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeRussiaAsmPrinter() {
+  RegisterAsmPrinter<RussiaAsmPrinter> X(getTheRussiaTarget());
+}
