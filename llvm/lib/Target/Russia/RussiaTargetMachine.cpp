84aed4f2aff16b63d8605fd507f5326681103adc
Vlad Lazar <lazar_2004@list.ru>
2026-03-28 17:58:05 +0300

    [Sim] 10. Add RussiaTargetMachine::getObjFileLowering

--- llvm/lib/Target/Russia/RussiaTargetMachine.cpp
+++ llvm/lib/Target/Russia/RussiaTargetMachine.cpp
@@ -1,6 +1,7 @@
 #include "RussiaTargetMachine.h"
 #include "Russia.h"
 #include "TargetInfo/RussiaTargetInfo.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
 #include "llvm/CodeGen/TargetPassConfig.h"
 #include "llvm/MC/TargetRegistry.h"
 #include <optional>
@@ -19,9 +20,10 @@ RussiaTargetMachine::RussiaTargetMachine(const Target &T, const Triple &TT,
                                          std::optional<Reloc::Model> RM,
                                          std::optional<CodeModel::Model> CM,
                                          CodeGenOptLevel OL, bool JIT)
-    : CodeGenTargetMachineImpl(
-          T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32", TT, CPU, FS, Options,
-          Reloc::Static, getEffectiveCodeModel(CM, CodeModel::Small), OL) {
+    : CodeGenTargetMachineImpl(T, "e-m:e-p:32:32-i8:8:32-i16:16:32-i64:64-n32",
+                               TT, CPU, FS, Options, Reloc::Static,
+                               getEffectiveCodeModel(CM, CodeModel::Small), OL),
+      TLOF(std::make_unique<TargetLoweringObjectFileELF>()) {
   RUSSIA_DUMP_CYAN
   initAsmInfo();
 }
@@ -50,4 +52,9 @@ public:
 TargetPassConfig *RussiaTargetMachine::createPassConfig(PassManagerBase &PM) {
   RUSSIA_DUMP_CYAN
   return new RussiaPassConfig(*this, PM);
+}
+
+TargetLoweringObjectFile *RussiaTargetMachine::getObjFileLowering() const {
+  RUSSIA_DUMP_CYAN
+  return TLOF.get();
 }
