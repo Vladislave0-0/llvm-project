dce830b9b6d93249fee9cf5a0ac27e58d8ce4412
Vlad Lazar <lazar_2004@list.ru>
2026-03-30 20:23:38 +0300

    [Sim] 22. Update Rus Instruction info

--- llvm/lib/Target/Rus/Rus.h
+++ llvm/lib/Target/Rus/Rus.h
@@ -5,13 +5,13 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
 
-#define RUS_DUMP(Color)                                                     \
+/*#define RUS_DUMP(Color)                                                     \
   {                                                                            \
     llvm::errs().changeColor(Color)                                            \
         << __func__ << "\n\t\t" << __FILE__ << ":" << __LINE__ << "\n";        \
     llvm::errs().changeColor(llvm::raw_ostream::WHITE);                        \
-  }
-// #define RUS_DUMP(Color) {}
+  }*/
+#define RUS_DUMP(Color) {}
 
 #define RUS_DUMP_RED RUS_DUMP(llvm::raw_ostream::RED)
 #define RUS_DUMP_GREEN RUS_DUMP(llvm::raw_ostream::GREEN)
