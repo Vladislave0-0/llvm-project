6d7ac6c9b649deae4af9336aaf44804dfc2361a4
Vlad Lazar <lazar_2004@list.ru>
2026-05-06 18:40:06 +0300

    [Sim] 39. Implement branch analysis and insertion

--- llvm/lib/Target/Rus/RusInstrInfo.cpp
+++ llvm/lib/Target/Rus/RusInstrInfo.cpp
@@ -58,3 +58,92 @@ void RusInstrInfo::loadRegFromStackSlot(MachineBasicBlock &MBB,
       .addFrameIndex(FrameIndex)
       .addImm(0);
 }
+
+bool RusInstrInfo::analyzeBranch(MachineBasicBlock &MBB,
+                                 MachineBasicBlock *&TBB,
+                                 MachineBasicBlock *&FBB,
+                                 SmallVectorImpl<MachineOperand> &Cond,
+                                 bool AllowModify) const {
+  TBB = FBB = nullptr;
+  Cond.clear();
+
+  MachineBasicBlock::iterator I = MBB.end();
+  while (I != MBB.begin()) {
+    --I;
+
+    if (I->isDebugInstr())
+      continue;
+
+    if (!I->isTerminator())
+      break;
+
+    unsigned Opc = I->getOpcode();
+    if (Opc == Rus::B) {
+      TBB = I->getOperand(0).getMBB();
+    } else if (Opc == Rus::BR_COND) {
+      FBB = TBB;
+      TBB = I->getOperand(1).getMBB();
+      Cond.push_back(I->getOperand(0));
+    } else {
+      return true;
+    }
+  }
+  return false;
+}
+
+unsigned RusInstrInfo::insertBranch(MachineBasicBlock &MBB,
+                                    MachineBasicBlock *TBB,
+                                    MachineBasicBlock *FBB,
+                                    ArrayRef<MachineOperand> Cond,
+                                    const DebugLoc &DL, int *BytesAdded) const {
+  assert(TBB && "insertBranch must not be told to insert a fallthrough");
+  assert(Cond.size() <= 1);
+
+  if (Cond.empty()) {
+    BuildMI(&MBB, DL, get(Rus::B)).addMBB(TBB);
+    if (BytesAdded)
+      *BytesAdded = 8;
+
+    return 1;
+  }
+
+  BuildMI(&MBB, DL, get(Rus::BR_COND)).add(Cond[0]).addMBB(TBB);
+  if (!FBB) {
+    if (BytesAdded)
+      *BytesAdded = 8;
+
+    return 1;
+  }
+
+  BuildMI(&MBB, DL, get(Rus::B)).addMBB(FBB);
+  if (BytesAdded)
+    *BytesAdded = 16;
+
+  return 2;
+}
+
+unsigned RusInstrInfo::removeBranch(MachineBasicBlock &MBB,
+                                    int *BytesRemoved) const {
+  MachineBasicBlock::iterator I = MBB.end();
+  unsigned Count = 0;
+
+  while (I != MBB.begin()) {
+    --I;
+
+    if (I->isDebugInstr())
+      continue;
+
+    unsigned Opc = I->getOpcode();
+    if (Opc != Rus::B && Opc != Rus::BR_COND)
+      break;
+
+    I->eraseFromParent();
+    I = MBB.end();
+    ++Count;
+  }
+
+  if (BytesRemoved)
+    *BytesRemoved = Count * 8;
+
+  return Count;
+}
